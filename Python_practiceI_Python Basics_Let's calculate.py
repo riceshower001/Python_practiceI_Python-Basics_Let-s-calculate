0c2bb22b0e6d6cc54d59ef74ae2cd2328da5c029
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-03 03:27:21 +0900

    Add files via upload

---
+++ Python_practiceI_Python Basics_Let's calculate.py
@@ -0,0 +1,8 @@
+# Please print the value of dividing 9 by 2.
+print(9/2)
+
+# Please print the value of 7 multiplied by 5.
+print(7*5)
+
+# Please print the remainder of dividing 5 by 2.
+print(5%2)
